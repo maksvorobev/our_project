cef9aebe00343d75c73f084f07da580090529912
maksvorobev <vorobev.maxim@phystech.edu>
2023-04-14 11:44:19 +0300

    add don't test files block.cpp and block.h

---
+++ headers/block.h
@@ -0,0 +1,40 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+#include <QWidget>
+#include <vector>
+#include "Signal.h"
+class Block: public QWidget
+{
+    Q_OBJECT
+public:
+    Block(QWidget *parent = nullptr);
+
+    const std::vector<Block*>&  getOutput();
+    const std::vector<Block*>& getInput();
+
+    bool addOutput(Block* block);
+    bool addInput(Block* block);
+
+    virtual void setSignal(Signal* signal);
+
+    virtual Signal* getSignal();
+
+    bool ready();
+
+    virtual void work();
+
+    virtual ~Block();
+
+protected:
+    std::vector<Signal*> all_signals;
+    std::vector<Block*> input;
+    std::vector<Block*> output;
+    int max_input {1};
+
+
+signals:
+
+};
+
+#endif // BLOCK_H
